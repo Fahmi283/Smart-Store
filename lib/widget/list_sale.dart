cb25e61a7a60cb512ca8522361d3a73b976be46f
Fadillah283 <fahmifadillah283@gmail.com>
2022-11-08 09:20:27 +0700

    add feature search

--- lib/widget/list_sale.dart
+++ lib/widget/list_sale.dart
@@ -24,6 +24,7 @@ class _ListSaleState extends State<ListSale> {
         backgroundColor: Colors.blue, content: Text(message.toString())));
   }
 
+  var searchResult = [];
   @override
   void initState() {
     searchController = TextEditingController();
@@ -57,101 +58,236 @@ class _ListSaleState extends State<ListSale> {
             ),
           );
         }
-        if (value.isNotEmpty) {
+        if (value.isNotEmpty || searchResult.isNotEmpty) {
           SmartDialog.dismiss();
           return Column(
             children: [
+              Container(
+                height: 45,
+                margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                child: TextField(
+                  onChanged: (valueController) {
+                    setState(() {
+                      searchResult = value
+                          .where((element) => element.name
+                              .toLowerCase()
+                              .contains(valueController.toLowerCase()))
+                          .toList();
+                    });
+                  },
+                  controller: searchController,
+                  decoration: const InputDecoration(
+                      hintText: "Search Data Penjualan",
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(25.0)))),
+                ),
+              ),
               Expanded(
                 child: Container(
                   margin: const EdgeInsets.symmetric(vertical: 10),
-                  child: ListView.separated(
-                    itemCount: value.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        margin: const EdgeInsets.only(left: 10, right: 10),
-                        child: ExpansionTile(
-                          leading: SizedBox(
-                              width: 30,
-                              child: Center(
-                                child: Text('${index + 1}.)'),
-                              )),
-                          title: Text(value[index].name),
-                          subtitle: Text(
-                              'Price : Rp. ${currency.format(value[index].sellingPrice)}'),
-                          children: [
-                            ListTile(
-                              onTap: () {},
-                              leading: SizedBox(width: 30, child: Container()),
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
+                  child: (searchResult.isEmpty)
+                      ? ListView.separated(
+                          itemCount: value.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              margin:
+                                  const EdgeInsets.only(left: 10, right: 10),
+                              child: ExpansionTile(
+                                leading: SizedBox(
+                                    width: 30,
+                                    child: Center(
+                                      child: Text('${index + 1}.)'),
+                                    )),
+                                title: Text(value[index].name),
+                                subtitle: Text(
+                                    'Price : Rp. ${currency.format(value[index].sellingPrice)}'),
                                 children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      Navigator.pushNamed(
-                                          context, EditSale.routeName,
-                                          arguments: value[index]);
-                                    },
-                                    icon: const Icon(Icons.edit),
-                                  ),
-                                  IconButton(
-                                    onPressed: () async {
-                                      showDialog(
-                                          barrierDismissible: false,
-                                          context: context,
-                                          builder: (BuildContext context) {
-                                            return AlertDialog(
-                                              title: const Text(
-                                                  'Ingin Menghapus Data?'),
-                                              content: const Text(
-                                                  'Anda hanya bisa Menghapus data yang telah anda tambahkan'),
-                                              actions: <Widget>[
-                                                TextButton(
-                                                  onPressed: () {
-                                                    Navigator.pop(context);
-                                                  },
-                                                  child: const Text('Batal'),
-                                                ),
-                                                TextButton(
-                                                  onPressed: () async {
-                                                    final result = await manager
-                                                        .delete(value[index]);
-                                                    manager.get();
-                                                    if (mounted) {}
-                                                    Navigator.pop(context);
-                                                    showNotification(
-                                                        context, result);
-                                                    if (mounted) {}
-                                                    Navigator.pop(context);
-                                                    showNotification(
-                                                        context, result);
-                                                  },
-                                                  child: const Text('Hapus'),
-                                                ),
-                                              ],
-                                            );
-                                          });
-                                    },
-                                    icon: const Icon(Icons.delete),
+                                  ListTile(
+                                    onTap: () {},
+                                    leading:
+                                        SizedBox(width: 30, child: Container()),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            Navigator.pushNamed(
+                                                context, EditSale.routeName,
+                                                arguments: value[index]);
+                                          },
+                                          icon: const Icon(Icons.edit),
+                                        ),
+                                        IconButton(
+                                          onPressed: () async {
+                                            showDialog(
+                                                barrierDismissible: false,
+                                                context: context,
+                                                builder:
+                                                    (BuildContext context) {
+                                                  return AlertDialog(
+                                                    title: const Text(
+                                                        'Ingin Menghapus Data?'),
+                                                    content: const Text(
+                                                        'Anda hanya bisa Menghapus data yang telah anda tambahkan'),
+                                                    actions: <Widget>[
+                                                      TextButton(
+                                                        onPressed: () {
+                                                          Navigator.pop(
+                                                              context);
+                                                        },
+                                                        child:
+                                                            const Text('Batal'),
+                                                      ),
+                                                      TextButton(
+                                                        onPressed: () async {
+                                                          final result =
+                                                              await manager
+                                                                  .delete(value[
+                                                                      index]);
+                                                          manager.get();
+                                                          if (mounted) {}
+                                                          Navigator.pop(
+                                                              context);
+                                                          showNotification(
+                                                              context, result);
+                                                          if (mounted) {}
+                                                          Navigator.pop(
+                                                              context);
+                                                          showNotification(
+                                                              context, result);
+                                                        },
+                                                        child:
+                                                            const Text('Hapus'),
+                                                      ),
+                                                    ],
+                                                  );
+                                                });
+                                          },
+                                          icon: const Icon(Icons.delete),
+                                        ),
+                                      ],
+                                    ),
+                                    subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('Quantity : ${value[index].sum}'),
+                                        Text('Date : ${value[index].date}'),
+                                      ],
+                                    ),
                                   ),
                                 ],
                               ),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const SizedBox(
+                            height: 5,
+                          ),
+                        )
+                      : ListView.separated(
+                          itemCount: searchResult.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              margin:
+                                  const EdgeInsets.only(left: 10, right: 10),
+                              child: ExpansionTile(
+                                leading: SizedBox(
+                                    width: 30,
+                                    child: Center(
+                                      child: Text('${index + 1}.)'),
+                                    )),
+                                title: Text(searchResult[index].name),
+                                subtitle: Text(
+                                    'Price : Rp. ${currency.format(searchResult[index].sellingPrice)}'),
                                 children: [
-                                  Text('Quantity : ${value[index].sum}'),
-                                  Text('Date : ${value[index].date}'),
+                                  ListTile(
+                                    onTap: () {},
+                                    leading:
+                                        SizedBox(width: 30, child: Container()),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            Navigator.pushNamed(
+                                                context, EditSale.routeName,
+                                                arguments: searchResult[index]);
+                                          },
+                                          icon: const Icon(Icons.edit),
+                                        ),
+                                        IconButton(
+                                          onPressed: () async {
+                                            showDialog(
+                                                barrierDismissible: false,
+                                                context: context,
+                                                builder:
+                                                    (BuildContext context) {
+                                                  return AlertDialog(
+                                                    title: const Text(
+                                                        'Ingin Menghapus Data?'),
+                                                    content: const Text(
+                                                        'Anda hanya bisa Menghapus data yang telah anda tambahkan'),
+                                                    actions: <Widget>[
+                                                      TextButton(
+                                                        onPressed: () {
+                                                          Navigator.pop(
+                                                              context);
+                                                        },
+                                                        child:
+                                                            const Text('Batal'),
+                                                      ),
+                                                      TextButton(
+                                                        onPressed: () async {
+                                                          final result =
+                                                              await manager.delete(
+                                                                  searchResult[
+                                                                      index]);
+                                                          manager.get();
+                                                          if (mounted) {}
+                                                          Navigator.pop(
+                                                              context);
+                                                          showNotification(
+                                                              context, result);
+                                                          if (mounted) {}
+                                                          Navigator.pop(
+                                                              context);
+                                                          showNotification(
+                                                              context, result);
+                                                        },
+                                                        child:
+                                                            const Text('Hapus'),
+                                                      ),
+                                                    ],
+                                                  );
+                                                });
+                                          },
+                                          icon: const Icon(Icons.delete),
+                                        ),
+                                      ],
+                                    ),
+                                    subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                            'Quantity : ${searchResult[index].sum}'),
+                                        Text(
+                                            'Date : ${searchResult[index].date}'),
+                                      ],
+                                    ),
+                                  ),
                                 ],
                               ),
-                            ),
-                          ],
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const SizedBox(
+                            height: 5,
+                          ),
                         ),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const SizedBox(
-                      height: 5,
-                    ),
-                  ),
                 ),
               ),
             ],
